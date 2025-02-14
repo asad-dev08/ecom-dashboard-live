d500b83ca5af86bb8933537027b1ca3d3b14c15d
asad-dev08 <asad.dev08@gmail.com>
2025-02-14 21:54:11 +0800

    fix

---
+++ api/db.js
@@ -0,0 +1,9 @@
+import jsonServer from 'json-server';
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+export default server;
